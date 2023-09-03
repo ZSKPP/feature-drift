11838c27b13656cebf4e4a22c04d4d757b223d3d
ZSKPP <143934389+ZSKPP@users.noreply.github.com>
2023-09-03 20:30:45 +0200

    Add files via upload

---
+++ DATA synthetic MOA/_Gradual_RBF.bat
@@ -0,0 +1,9 @@
+java -cp moa.jar moa.DoTask "WriteStreamToARFFFile -s (ConceptDriftStream -s (generators.RandomRBFGenerator -a 20) -d (generators.RandomRBFGeneratorDrift -i 10 -s 2.0 -a 20) -p 50000 -w 20000) -f Gradual_RBF_10.arff -m 100000"
+
+java -cp moa.jar moa.DoTask "WriteStreamToARFFFile -s (ConceptDriftStream -s (generators.RandomRBFGenerator -a 20) -d (generators.RandomRBFGeneratorDrift -i 20 -s 2.0 -a 20) -p 50000 -w 20000) -f Gradual_RBF_20.arff -m 100000"
+
+java -cp moa.jar moa.DoTask "WriteStreamToARFFFile -s (ConceptDriftStream -s (generators.RandomRBFGenerator -a 20) -d (generators.RandomRBFGeneratorDrift -i 30 -s 2.0 -a 20) -p 50000 -w 20000) -f Gradual_RBF_30.arff -m 100000"
+
+java -cp moa.jar moa.DoTask "WriteStreamToARFFFile -s (ConceptDriftStream -s (generators.RandomRBFGenerator -a 20) -d (generators.RandomRBFGeneratorDrift -i 40 -s 2.0 -a 20) -p 50000 -w 20000) -f Gradual_RBF_40.arff -m 100000"
+
+java -cp moa.jar moa.DoTask "WriteStreamToARFFFile -s (ConceptDriftStream -s (generators.RandomRBFGenerator -a 20) -d (generators.RandomRBFGeneratorDrift -i 50 -s 2.0 -a 20) -p 50000 -w 20000) -f Gradual_RBF_50.arff -m 100000"
